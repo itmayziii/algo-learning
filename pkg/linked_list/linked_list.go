73e4bcfc5f4c31dffe79d4628b8c4fe6d028cc7b
Tommy May <tommymay37@gmail.com>
2024-06-09 11:09:23 -0400

    feat(linked_list): added Length and Unshift methods

--- pkg/linked_list/linked_list.go
+++ pkg/linked_list/linked_list.go
@@ -11,32 +11,49 @@ type Node struct {
 	Data []byte
 }
 
+// Traverse returns the contents of the linked list in a readable format. O(N)
+// i.e. A linked list with the head value of "A" and the second node value of "B" would produce "A -> B"
 func (n *Node) Traverse() string {
 	current := n
-
 	var traversalStr strings.Builder
 	for i := 0; current != nil; i++ {
 		if i != 0 {
 			traversalStr.WriteString(fmt.Sprintf(" -> "))
 		}
 		traversalStr.WriteString(fmt.Sprintf("%s", current.Data))
-
 		current = current.Next
 	}
 
 	return traversalStr.String()
 }
 
+// Search returns whether a given value is in the linked list. O(N)
 func (n *Node) Search(needle []byte) bool {
 	current := n
-
 	for current != nil {
 		if bytes.Equal(current.Data, needle) {
 			return true
 		}
-
 		current = current.Next
 	}
 
 	return false
+}
+
+// Length returns how many nodes are in the linked list. O(N)
+func (n *Node) Length() int {
+	current := n
+	count := 0
+	for current != nil {
+		count++
+		current = current.Next
+	}
+
+	return count
+}
+
+// Unshift adds a new node as the head of the linked list and returns the new head. O(N)
+func (n *Node) Unshift(newNode *Node) *Node {
+	newNode.Next = n
+	return newNode
 }
--- pkg/linked_list/linked_list_test.go
+++ pkg/linked_list/linked_list_test.go
@@ -149,3 +149,93 @@ func TestNode_Search(t *testing.T) {
 		})
 	}
 }
+
+func TestNode_Length(t *testing.T) {
+	head := &linked_list.Node{
+		Data: []byte("A"),
+	}
+	second := &linked_list.Node{
+		Data: []byte("B"),
+	}
+	third := &linked_list.Node{
+		Data: []byte("C"),
+	}
+	head.Next = second
+	second.Next = third
+
+	tests := []struct {
+		list     *linked_list.Node
+		expected int
+	}{
+		{
+			head,
+			3,
+		},
+		{
+			second,
+			2,
+		},
+		{
+			third,
+			1,
+		},
+		{
+			nil,
+			0,
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			t.Parallel()
+
+			actual := tt.list.Length()
+			if actual != tt.expected {
+				t.Errorf("actual %d, expected %d", actual, tt.expected)
+			}
+		})
+	}
+}
+
+func TestNode_Unshift(t *testing.T) {
+	head := &linked_list.Node{
+		Data: []byte("A"),
+	}
+	second := &linked_list.Node{
+		Data: []byte("B"),
+	}
+	third := &linked_list.Node{
+		Data: []byte("C"),
+	}
+	head.Next = second
+	second.Next = third
+
+	tests := []struct {
+		list *linked_list.Node
+	}{
+		{head},
+		{second},
+		{third},
+		{nil},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
+			t.Parallel()
+
+			newNode := &linked_list.Node{
+				Data: []byte("0"),
+			}
+			actual := tt.list.Unshift(newNode)
+			if actual != newNode {
+				t.Errorf("first node in list: %s is not the expected new node: %s", actual.Data, newNode.Data)
+			}
+			if actual.Next != tt.list {
+				t.Errorf("newly added first node: %s does not refrence the old head as the next node: %s",
+					actual.Data,
+					tt.list.Data,
+				)
+			}
+		})
+	}
+}
