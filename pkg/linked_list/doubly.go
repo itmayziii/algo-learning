eed07d93835011c402c3fac6fc8ce394f88f3cf1
Tommy May <tommymay37@gmail.com>
2024-06-18 18:45:41 -0400

    feat(linked lists): added doubly linked list with tests

--- pkg/linked_list/doubly.go
+++ pkg/linked_list/doubly.go
@@ -5,22 +5,23 @@ import (
 	"strings"
 )
 
-// Singly represents a singly linked list.
-type Singly struct {
-	head   *singlyNode
-	tail   *singlyNode
+// Doubly represents a doubly linked list.
+type Doubly struct {
+	head   *doublyNode
+	tail   *doublyNode
 	length int
 }
 
-// singlyNode represents a node in a singly linked list.
-type singlyNode struct {
-	next  *singlyNode
+// doublyNode represents a node in a doubly linked list.
+type doublyNode struct {
+	next  *doublyNode
+	prev  *doublyNode
 	value string
 }
 
 // Traverse returns the contents of the linked list in a readable format. O(N)
 // i.e. A linked list with the head value of "A" and the second singlyNode value of "B" would produce "A -> B"
-func (ll *Singly) Traverse() string {
+func (ll *Doubly) Traverse() string {
 	current := ll.head
 	var traversalStr strings.Builder
 	for i := 0; current != nil; i++ {
@@ -36,7 +37,7 @@ func (ll *Singly) Traverse() string {
 }
 
 // Search returns whether a given value is in the linked list. O(N)
-func (ll *Singly) Search(needle string) bool {
+func (ll *Doubly) Search(needle string) bool {
 	current := ll.head
 	for current != nil {
 		if current.value == needle {
@@ -52,15 +53,20 @@ func (ll *Singly) Search(needle string) bool {
 //
 // O(1) - would be O(N) if we did not maintain the length internally and would instead have to walk the N sized
 // list to count the values.
-func (ll *Singly) Length() int {
+func (ll *Doubly) Length() int {
 	return ll.length
 }
 
 // Unshift adds a new node as the head of the linked list.
 //
 // O(1)
-func (ll *Singly) Unshift(value string) {
-	newNode := &singlyNode{value: value, next: ll.head}
+func (ll *Doubly) Unshift(value string) {
+	newNode := &doublyNode{value: value, next: ll.head}
+
+	if ll.head != nil {
+		ll.head.prev = newNode
+	}
+
 	ll.head = newNode
 	ll.length++
 	if ll.head.next == nil {
@@ -71,7 +77,7 @@ func (ll *Singly) Unshift(value string) {
 // Shift removes the head of a linked list and returns the value of the removed node.
 //
 // O(1)
-func (ll *Singly) Shift() string {
+func (ll *Doubly) Shift() string {
 	if ll.head == nil {
 		return ""
 	}
@@ -80,10 +86,16 @@ func (ll *Singly) Shift() string {
 	ll.head = oldHead.next
 	ll.length--
 
-	if ll.head == nil {
-		ll.tail = nil
-	} else if ll.head.next == nil {
-		ll.tail = ll.head
+	// free memory
+	oldHead.next = nil
+	oldHead.prev = nil
+
+	if ll.head != nil {
+		ll.head.prev = nil
+
+		if ll.head.next == nil {
+			ll.tail = ll.head
+		}
 	}
 
 	return oldHead.value
@@ -93,8 +105,8 @@ func (ll *Singly) Shift() string {
 //
 // O(1) - It would be O(N) if we did not maintain a reference to the current tail node as we would have to loop
 // through the N sized list to find the tail.
-func (ll *Singly) Push(value string) {
-	newNode := &singlyNode{value: value}
+func (ll *Doubly) Push(value string) {
+	newNode := &doublyNode{value: value, prev: ll.tail}
 
 	if ll.tail != nil {
 		ll.tail.next = newNode
@@ -109,14 +121,14 @@ func (ll *Singly) Push(value string) {
 
 // Pop removes the tail of a linked list and returns its value.
 //
-// O(N) - In a doubly linked list this operation could be O(1) because we could jump to the tail and set the previous
-// node.next = nil, but in a singly linked list we must walk the N sized list so that we can get penultimate node.
-func (ll *Singly) Pop() string {
+// O(1) - In a singly linked list this operation would be O(N) because we could not jump to the tail and set
+// the previous.next = nil
+func (ll *Doubly) Pop() string {
 	if ll.tail == nil {
 		return ""
 	}
 
-	if ll.head.next == nil {
+	if ll.head.next == nil || ll.tail.prev == nil {
 		value := ll.head.value
 		ll.head = nil
 		ll.tail = nil
@@ -124,29 +136,24 @@ func (ll *Singly) Pop() string {
 		return value
 	}
 
-	previous := ll.head
-	current := ll.head
-	for current != nil {
-		if current.next == nil {
-			previous.next = nil
-			ll.tail = previous
-			ll.length--
-			return current.value
-		}
+	oldTail := ll.tail
+	ll.tail = oldTail.prev
+	ll.tail.next = nil
+	ll.length--
 
-		previous = current
-		current = current.next
-	}
+	// free memory
+	oldTail.next = nil
+	oldTail.prev = nil
 
-	return ""
+	return oldTail.value
 }
 
 // Insert inserts a new node at a given position and returns the position the value was inserted at or -1 if no
 // insertion occurred.
 //
 // O(N)
-func (ll *Singly) Insert(value string, position int) int {
-	if position < 0 {
+func (ll *Doubly) Insert(value string, position int) int {
+	if position < 0 || position > ll.length {
 		return -1
 	}
 
@@ -155,27 +162,22 @@ func (ll *Singly) Insert(value string, position int) int {
 		return 0
 	}
 
-	previous := ll.head
-	current := ll.head
-	for i := 0; i <= position; i++ {
-		// We are 2 past the tail node, time to give up
-		if previous == nil {
-			return -1
-		}
+	if position == ll.length {
+		ll.Push(value)
+		return position
+	}
 
+	current := ll.head
+	for i := 0; i <= position && current != nil; i++ {
 		if i != position {
-			previous = current
-			if current != nil {
-				current = current.next
-			}
+			current = current.next
 			continue
 		}
 
-		newNode := &singlyNode{value: value, next: current}
-		if current == nil {
-			ll.tail = newNode
-		}
-		previous.next = newNode
+		newNode := &doublyNode{value: value, next: current, prev: current.prev}
+		current.prev.next = newNode
+		current.prev = newNode
+
 		ll.length++
 		return i
 	}
@@ -186,8 +188,8 @@ func (ll *Singly) Insert(value string, position int) int {
 // Delete deletes a node and returns the position of the deleted node or -1 if no deletion occurred.
 //
 // O(N)
-func (ll *Singly) Delete(position int) int {
-	if position < 0 || ll.length == 0 {
+func (ll *Doubly) Delete(position int) int {
+	if position < 0 || ll.length == 0 || position >= ll.length {
 		return -1
 	}
 
@@ -196,20 +198,28 @@ func (ll *Singly) Delete(position int) int {
 		return 0
 	}
 
-	previous := ll.head
+	if position == ll.length-1 {
+		ll.Pop()
+		return position
+	}
+
 	current := ll.head
 	for i := 0; i <= position && current != nil; i++ {
 		if i != position {
-			previous = current
 			current = current.next
 			continue
 		}
 
-		previous.next = current.next
-		ll.length--
-		if previous.next == nil {
-			ll.tail = previous
+		current.prev.next = current.next
+		if current.next != nil {
+			current.next.prev = current.prev
 		}
+		ll.length--
+
+		// free memory
+		current.next = nil
+		current.prev = nil
+
 		return i
 	}
 
@@ -219,7 +229,7 @@ func (ll *Singly) Delete(position int) int {
 // First returns the value of the first node in the list.
 //
 // O(1)
-func (ll *Singly) First() string {
+func (ll *Doubly) First() string {
 	if ll.head == nil {
 		return ""
 	}
@@ -230,7 +240,7 @@ func (ll *Singly) First() string {
 // Last returns the value of the last node in the list.
 //
 // O(1)
-func (ll *Singly) Last() string {
+func (ll *Doubly) Last() string {
 	if ll.tail == nil {
 		return ""
 	}
@@ -239,11 +249,11 @@ func (ll *Singly) Last() string {
 }
 
 // String implements the Stringer interface
-func (ll *Singly) String() string {
+func (ll *Doubly) String() string {
 	return ll.Traverse()
 }
 
 // String implements the Stringer interface
-func (n *singlyNode) String() string {
+func (n *doublyNode) String() string {
 	return n.value
 }
--- pkg/linked_list/doubly_test.go
+++ pkg/linked_list/doubly_test.go
@@ -6,15 +6,15 @@ import (
 	"testing"
 )
 
-func TestSingly_Traverse(t *testing.T) {
+func TestDoubly_Traverse(t *testing.T) {
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		expected string
 	}{
-		{newSingly("A", "B", "C"), "A -> B -> C"},
-		{newSingly("B", "C"), "B -> C"},
-		{newSingly("C"), "C"},
-		{newSingly("A", "B", "C", "X", "Y", "Z"), "A -> B -> C -> X -> Y -> Z"},
+		{newDoubly("A", "B", "C"), "A -> B -> C"},
+		{newDoubly("B", "C"), "B -> C"},
+		{newDoubly("C"), "C"},
+		{newDoubly("A", "B", "C", "X", "Y", "Z"), "A -> B -> C -> X -> Y -> Z"},
 	}
 
 	for i, tt := range tests {
@@ -29,20 +29,20 @@ func TestSingly_Traverse(t *testing.T) {
 	}
 }
 
-func TestSingly_Search(t *testing.T) {
+func TestDoubly_Search(t *testing.T) {
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		needle   string
 		expected bool
 	}{
-		{newSingly("A", "B", "C"), "A", true},
-		{newSingly("A", "B", "C"), "B", true},
-		{newSingly("A", "B", "C"), "C", true},
-		{newSingly("A", "B", "C"), "D", false},
-		{newSingly("A", "B", "C"), "Z", false},
-		{newSingly(), "A", false},
-		{newSingly(), "B", false},
-		{newSingly(), "C", false},
+		{newDoubly("A", "B", "C"), "A", true},
+		{newDoubly("A", "B", "C"), "B", true},
+		{newDoubly("A", "B", "C"), "C", true},
+		{newDoubly("A", "B", "C"), "D", false},
+		{newDoubly("A", "B", "C"), "Z", false},
+		{newDoubly(), "A", false},
+		{newDoubly(), "B", false},
+		{newDoubly(), "C", false},
 	}
 
 	for i, tt := range tests {
@@ -57,17 +57,17 @@ func TestSingly_Search(t *testing.T) {
 	}
 }
 
-func TestSingly_Length(t *testing.T) {
+func TestDoubly_Length(t *testing.T) {
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		expected int
 	}{
-		{newSingly(), 0},
-		{newSingly("A"), 1},
-		{newSingly("A", "B"), 2},
-		{newSingly("A", "B", "B"), 3},
-		{newSingly("A", "B", "B", "B"), 4},
-		{newSingly("A", "B", "B", "B", "Z"), 5},
+		{newDoubly(), 0},
+		{newDoubly("A"), 1},
+		{newDoubly("A", "B"), 2},
+		{newDoubly("A", "B", "B"), 3},
+		{newDoubly("A", "B", "B", "B"), 4},
+		{newDoubly("A", "B", "B", "B", "Z"), 5},
 	}
 
 	for i, tt := range tests {
@@ -82,15 +82,15 @@ func TestSingly_Length(t *testing.T) {
 	}
 }
 
-func TestSingly_Unshift(t *testing.T) {
+func TestDoubly_Unshift(t *testing.T) {
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		value    string
 		expected string
 	}{
-		{newSingly("A", "B", "C"), "Z", "Z -> A -> B -> C"},
-		{newSingly("B", "C"), "A", "A -> B -> C"},
-		{newSingly("C"), "B", "B -> C"},
+		{newDoubly("A", "B", "C"), "Z", "Z -> A -> B -> C"},
+		{newDoubly("B", "C"), "A", "A -> B -> C"},
+		{newDoubly("C"), "B", "B -> C"},
 	}
 
 	for i, tt := range tests {
@@ -106,7 +106,7 @@ func TestSingly_Unshift(t *testing.T) {
 	}
 }
 
-func TestSingly_Shift(t *testing.T) {
+func TestDoubly_Shift(t *testing.T) {
 	type expected struct {
 		value     string
 		traversal string
@@ -114,13 +114,13 @@ func TestSingly_Shift(t *testing.T) {
 	}
 
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		expected expected
 	}{
-		{newSingly("A", "B", "C"), expected{"A", "B -> C", 2}},
-		{newSingly("B", "C"), expected{"B", "C", 1}},
-		{newSingly("C"), expected{"C", "", 0}},
-		{newSingly(), expected{"", "", 0}},
+		{newDoubly("A", "B", "C"), expected{"A", "B -> C", 2}},
+		{newDoubly("B", "C"), expected{"B", "C", 1}},
+		{newDoubly("C"), expected{"C", "", 0}},
+		{newDoubly(), expected{"", "", 0}},
 	}
 
 	for i, tt := range tests {
@@ -145,20 +145,20 @@ func TestSingly_Shift(t *testing.T) {
 	}
 }
 
-func TestSingly_Push(t *testing.T) {
+func TestDoubly_Push(t *testing.T) {
 	type expected struct {
 		traversal string
 		length    int
 	}
 
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		newTail  string
 		expected expected
 	}{
-		{newSingly("A", "B", "C"), "Z", expected{"A -> B -> C -> Z", 4}},
-		{newSingly("B", "C"), "Z", expected{"B -> C -> Z", 3}},
-		{newSingly("C"), "Z", expected{"C -> Z", 2}},
+		{newDoubly("A", "B", "C"), "Z", expected{"A -> B -> C -> Z", 4}},
+		{newDoubly("B", "C"), "Z", expected{"B -> C -> Z", 3}},
+		{newDoubly("C"), "Z", expected{"C -> Z", 2}},
 	}
 
 	for i, tt := range tests {
@@ -179,7 +179,7 @@ func TestSingly_Push(t *testing.T) {
 	}
 }
 
-func TestSingly_Pop(t *testing.T) {
+func TestDoubly_Pop(t *testing.T) {
 	type expected struct {
 		value     string
 		traversal string
@@ -187,14 +187,14 @@ func TestSingly_Pop(t *testing.T) {
 	}
 
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		expected expected
 	}{
-		{newSingly("A", "B", "C"), expected{"C", "A -> B", 2}},
-		{newSingly("B", "C"), expected{"C", "B", 1}},
-		{newSingly("C"), expected{"C", "", 0}},
-		{newSingly("A", "B"), expected{"B", "A", 1}},
-		{newSingly(""), expected{"", "", 0}},
+		{newDoubly("A", "B", "C"), expected{"C", "A -> B", 2}},
+		{newDoubly("B", "C"), expected{"C", "B", 1}},
+		{newDoubly("C"), expected{"C", "", 0}},
+		{newDoubly("A", "B"), expected{"B", "A", 1}},
+		{newDoubly(""), expected{"", "", 0}},
 	}
 
 	for i, tt := range tests {
@@ -219,7 +219,7 @@ func TestSingly_Pop(t *testing.T) {
 	}
 }
 
-func TestSingly_Insert(t *testing.T) {
+func TestDoubly_Insert(t *testing.T) {
 	type expected struct {
 		value     int
 		traversal string
@@ -227,37 +227,37 @@ func TestSingly_Insert(t *testing.T) {
 	}
 
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		value    string
 		position int
 		expected expected
 	}{
-		{newSingly("A", "B", "C"), "AA", 0, expected{
+		{newDoubly("A", "B", "C"), "AA", 0, expected{
 			0,
 			"AA -> A -> B -> C",
 			4,
 		}},
-		{newSingly("A", "B", "C"), "BB", 1, expected{
+		{newDoubly("A", "B", "C"), "BB", 1, expected{
 			1,
 			"A -> BB -> B -> C",
 			4,
 		}},
-		{newSingly("A", "B", "C"), "CC", 2, expected{
+		{newDoubly("A", "B", "C"), "CC", 2, expected{
 			2,
 			"A -> B -> CC -> C",
 			4,
 		}},
-		{newSingly("A", "B", "C"), "D", 3, expected{
+		{newDoubly("A", "B", "C"), "D", 3, expected{
 			3,
 			"A -> B -> C -> D",
 			4,
 		}},
-		{newSingly("A", "B", "C"), "E", 4, expected{
+		{newDoubly("A", "B", "C"), "E", 4, expected{
 			-1,
 			"A -> B -> C",
 			3,
 		}},
-		{newSingly("A", "B", "C"), "X", -3, expected{
+		{newDoubly("A", "B", "C"), "X", -3, expected{
 			-1,
 			"A -> B -> C",
 			3,
@@ -286,7 +286,7 @@ func TestSingly_Insert(t *testing.T) {
 	}
 }
 
-func TestSingly_Delete(t *testing.T) {
+func TestDoubly_Delete(t *testing.T) {
 	type expected struct {
 		value     int
 		traversal string
@@ -294,18 +294,18 @@ func TestSingly_Delete(t *testing.T) {
 	}
 
 	tests := []struct {
-		list     *linked_list.Singly
+		list     *linked_list.Doubly
 		position int
 		expected expected
 	}{
-		{newSingly("A", "B", "C"), 0, expected{0, "B -> C", 2}},
-		{newSingly("A", "B", "C"), 1, expected{1, "A -> C", 2}},
-		{newSingly("A", "B", "C"), 2, expected{2, "A -> B", 2}},
-		{newSingly("A", "B", "C"), 3, expected{-1, "A -> B -> C", 3}},
-		{newSingly("X", "Y", "Z"), 4, expected{-1, "X -> Y -> Z", 3}},
-		{newSingly(), 4, expected{-1, "", 0}},
-		{newSingly(), 0, expected{-1, "", 0}},
-		{newSingly("A"), 0, expected{0, "", 0}},
+		{newDoubly("A", "B", "C"), 0, expected{0, "B -> C", 2}},
+		{newDoubly("A", "B", "C"), 1, expected{1, "A -> C", 2}},
+		{newDoubly("A", "B", "C"), 2, expected{2, "A -> B", 2}},
+		{newDoubly("A", "B", "C"), 3, expected{-1, "A -> B -> C", 3}},
+		{newDoubly("X", "Y", "Z"), 4, expected{-1, "X -> Y -> Z", 3}},
+		{newDoubly(), 4, expected{-1, "", 0}},
+		{newDoubly(), 0, expected{-1, "", 0}},
+		{newDoubly("A"), 0, expected{0, "", 0}},
 	}
 
 	for i, tt := range tests {
@@ -330,9 +330,9 @@ func TestSingly_Delete(t *testing.T) {
 	}
 }
 
-// I don't generally believe in tests like this but there is enough internal state being kept by the singly
+// I don't generally believe in tests like this but there is enough internal state being kept by the doubly
 // linked list that it is important to test the result of operations after other operations have occurred.
-func TestSingly_Integration(t *testing.T) {
+func TestDoubly_Integration(t *testing.T) {
 	t.Parallel()
 
 	type expected struct {
@@ -343,15 +343,15 @@ func TestSingly_Integration(t *testing.T) {
 	}
 
 	type change struct {
-		operation func(ll *linked_list.Singly)
+		operation func(ll *linked_list.Doubly)
 		expected  expected
 	}
 
 	test := struct {
-		list    *linked_list.Singly
+		list    *linked_list.Doubly
 		changes []change
 	}{
-		newSingly("A", "B", "C"),
+		newDoubly("A", "B", "C"),
 		[]change{
 			{
 				operation: nil,
@@ -363,7 +363,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Shift()
 				},
 				expected: expected{
@@ -374,7 +374,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Pop()
 				},
 				expected: expected{
@@ -385,7 +385,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Unshift("Z")
 				},
 				expected: expected{
@@ -396,7 +396,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Delete(4)
 				},
 				expected: expected{
@@ -407,7 +407,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Delete(1)
 				},
 				expected: expected{
@@ -418,7 +418,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Pop()
 				},
 				expected: expected{
@@ -429,7 +429,7 @@ func TestSingly_Integration(t *testing.T) {
 				},
 			},
 			{
-				operation: func(ll *linked_list.Singly) {
+				operation: func(ll *linked_list.Doubly) {
 					ll.Insert("X", 0)
 				},
 				expected: expected{
@@ -469,10 +469,10 @@ func TestSingly_Integration(t *testing.T) {
 	}
 }
 
-// newSingly returns a new linked list as some of the linked list methods are mutating, and we don't want
+// newDoubly returns a new linked list as some of the linked list methods are mutating, and we don't want
 // to have shared state between test cases.
-func newSingly(values ...string) *linked_list.Singly {
-	ll := &linked_list.Singly{}
+func newDoubly(values ...string) *linked_list.Doubly {
+	ll := &linked_list.Doubly{}
 
 	for _, value := range values {
 		ll.Push(value)
